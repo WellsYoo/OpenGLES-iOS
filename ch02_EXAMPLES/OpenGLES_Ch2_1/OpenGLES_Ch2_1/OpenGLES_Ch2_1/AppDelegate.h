20aa1eea33da2fd64b55f9c2a8645cbaab2943aa
YWH <ywh@meitu.com>
2018-07-30 20:05:04 +0800

    【Feature】first commit

---
+++ ch02_EXAMPLES/OpenGLES_Ch2_1/OpenGLES_Ch2_1/OpenGLES_Ch2_1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenGLES_Ch2_1
+//
+//  Created by YWH on 2018/7/30.
+//  Copyright © 2018年 wells. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ch02_EXAMPLES/OpenGLES_Ch2_1/OpenGLES_Ch2_1/OpenGLES_Ch2_1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenGLES_Ch2_1
+//
+//  Created by YWH on 2018/7/30.
+//  Copyright © 2018年 wells. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
